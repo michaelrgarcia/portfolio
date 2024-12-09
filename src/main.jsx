b9866c6b69387d80d98d73ce7d79dceead5a8dc8
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2024-12-09 03:50:51 -0800

    Fix imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import Background from "./components/Background.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Background />
+    <main>Unfinished.</main>
+  </StrictMode>
+);
